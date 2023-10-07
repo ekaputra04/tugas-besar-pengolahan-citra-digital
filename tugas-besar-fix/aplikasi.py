91204c9f72b2de963cc318a193e0828408861e83
ekaputra04 <ekaputrajuniawan@gmail.com>
2023-10-07 19:06:02 +0800

    menambahkan operasi image blending

--- tugas-besar-fix/aplikasi.py
+++ tugas-besar-fix/aplikasi.py
@@ -17,12 +17,16 @@ def add_images(image1, image2):
     img1 = cv2.imread(image1)
     img2 = cv2.imread(image2)
 
-    # Pastikan ukuran kedua gambar sama
-    if img1.shape == img2.shape:
-        result = cv2.add(img1, img2)
-        return result
-    else:
-        return None
+    # Pastikan kedua gambar memiliki ukuran yang sama
+    if img1.shape != img2.shape:
+        # Menyesuaikan ukuran gambar agar sama
+        width = min(img1.shape[1], img2.shape[1])
+        height = min(img1.shape[0], img2.shape[0])
+        img1 = cv2.resize(img1, (width, height))
+        img2 = cv2.resize(img2, (width, height))
+
+    result = cv2.add(img1, img2)
+    return result
 
 # pengurangan 2 citra
 def subtract_images(image1, image2):
@@ -30,13 +34,17 @@ def subtract_images(image1, image2):
     img1 = cv2.imread(image1)
     img2 = cv2.imread(image2)
 
-    # Pastikan ukuran kedua gambar sama
-    if img1.shape == img2.shape:
-        result = cv2.subtract(img1, img2)
-        return result
-    else:
-        return None
+    # Pastikan kedua gambar memiliki ukuran yang sama
+    if img1.shape != img2.shape:
+        # Menyesuaikan ukuran gambar agar sama
+        width = min(img1.shape[1], img2.shape[1])
+        height = min(img1.shape[0], img2.shape[0])
+        img1 = cv2.resize(img1, (width, height))
+        img2 = cv2.resize(img2, (width, height))
 
+    result = cv2.subtract(img1, img2)
+    return result
+    
 # penjumlahan citra dengan konstanta
 def add_constant(image_path, constant_value):
     # Baca gambar
@@ -55,8 +63,41 @@ def subtract_constant(image_path, constant_value):
     result_image = cv2.subtract(image, (constant_value, constant_value, constant_value, 0))
     return result_image
 
-# perkalian citra
+# perkalian 2 citra
+def multiplication_images(image1, image2):
+    # Baca gambar
+    img1 = cv2.imread(image1)
+    img2 = cv2.imread(image2)
+
+    # Pastikan kedua gambar memiliki ukuran yang sama
+    if img1.shape != img2.shape:
+        # Menyesuaikan ukuran gambar agar sama
+        width = min(img1.shape[1], img2.shape[1])
+        height = min(img1.shape[0], img2.shape[0])
+        img1 = cv2.resize(img1, (width, height))
+        img2 = cv2.resize(img2, (width, height))
+
+    result = cv2.multiply(img1, img2)
+    return result
+    
 # image blanding
+def blending_images(image1, image2, alpha, beta):
+    # Baca gambar
+    img1 = cv2.imread(image1)
+    img2 = cv2.imread(image2)
+
+    # Pastikan kedua gambar memiliki ukuran yang sama
+    if img1.shape != img2.shape:
+        # Menyesuaikan ukuran gambar agar sama
+        width = min(img1.shape[1], img2.shape[1])
+        height = min(img1.shape[0], img2.shape[0])
+        img1 = cv2.resize(img1, (width, height))
+        img2 = cv2.resize(img2, (width, height))
+    
+    result = cv2.addWeighted(img1, alpha, img2, beta, 0)
+    return result
+
+# logika operator
 
 @app.route('/')
 def index():
@@ -76,33 +117,49 @@ def result():
     img_url1 = f'/static/uploads/{filename1}'
     img_url2 = f'/static/uploads/{filename2}'
 
+    # Ambil jenis operasi yang dipilih
+    operation = request.form.get('operation')  
+    # Ambil nilai konstanta dari form dengan default 0 jika tidak ada
+    constant_value = int(request.form.get('constant-value', '0'))
+    # Ambil nilai alpha dari form dengan default 0.5 jika tidak ada
+    alpha = float(request.form.get('alpha', '0.5'))
+    # Ambil nilai beta dari form dengan default 0.5 jika tidak ada
+    beta = float(request.form.get('beta', '0.5'))
+
+
     # Operasikan gambar dan simpan hasilnya
     # penjumlahan dua citra
-    operation = request.form.get('operation')  # Ambil jenis operasi yang dipilih
-    # Ambil nilai konstanta dari form dengan default 0 jika tidak ada
-    constant_value = int(request.form.get('constant-value', 0))
-
     if operation == 'addition':
         result_img = add_images(os.path.join(app.config['UPLOAD_FOLDER'], filename1), os.path.join(app.config['UPLOAD_FOLDER'], filename2))
         result_filename = f'result_add_{filename1}_{filename2}'
         operation_label = "Penjumlahan Dua Citra"
+    # penjumlahan citra dengan konstanta
     elif operation == 'addition-constanta':
-        # Operasikan gambar dan simpan hasilnya (penjumlahan citra dengan konstanta)
-        # gambar 1
         result_img = add_constant(os.path.join(app.config['UPLOAD_FOLDER'], filename1), constant_value)
-        result_filename = f'result_add_const_{filename1}_{constant_value}'
+        result_filename = f'result_add_const_{constant_value}_{filename1}'
         operation_label = "Penjumlahan dengan Konstanta"
+    # pengurangan dua citra
     elif operation == 'subtraction':
         result_img = subtract_images(os.path.join(app.config['UPLOAD_FOLDER'], filename1), os.path.join(app.config['UPLOAD_FOLDER'], filename2))
         result_filename = f'result_subtraction_{filename1}_{filename2}'
         operation_label = "Pengurangan Dua Citra"
+    # pengurangan citra dengan konstanta
     elif operation == 'subtraction-constanta':
-        # Operasikan gambar dan simpan hasilnya (penjumlahan citra dengan konstanta)
-        # gambar 1
         result_img = subtract_constant(os.path.join(app.config['UPLOAD_FOLDER'], filename1), constant_value)
-        result_filename = f'result_substract_const_{filename1}_{constant_value}'
+        result_filename = f'result_substract_const_{constant_value}_{filename1}'
         operation_label = "Pengurangan dengan Konstanta"
-    
+    # perkalian dua citra
+    elif operation == 'multiplication':
+        result_img = multiplication_images(os.path.join(app.config['UPLOAD_FOLDER'], filename1), constant_value)
+        result_filename = f'result_multiplication_{filename1}_{filename2}'
+        operation_label = "Perkalian Dua Citra"
+    # image blanding
+    elif operation == 'img-blending':
+        result_img = blending_images(os.path.join(app.config['UPLOAD_FOLDER'], filename1), os.path.join(app.config['UPLOAD_FOLDER'], filename2), alpha, beta)
+        result_filename = f'result_blending_{filename1}_{filename2}'
+        operation_label = "Images Blanding"
+    # logika operator
+
     result_path = os.path.join(app.config['UPLOAD_FOLDER'], result_filename)
 
     cv2.imwrite(result_path, result_img)
